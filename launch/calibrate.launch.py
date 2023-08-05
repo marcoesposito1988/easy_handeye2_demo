0a82a5d5ea6988ef572be902c6f93be2b8c8a86a
Marco Esposito <marcoesposito1988@gmail.com>
2023-08-05 12:15:46 +0200

    WIP modernizing & adding moveit

--- launch/calibrate.launch.py
+++ launch/calibrate.launch.py
@@ -1,31 +1,33 @@
 import os
 
 from ament_index_python import get_package_share_directory
+from easy_handeye2.common_launch import arg_calibration_type, arg_automatic_robot_movement, arg_translation_delta_meters, \
+    arg_rotation_delta_degrees, arg_max_velocity_scaling, arg_max_acceleration_scaling
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 
-from easy_handeye2.common_launch import arg_calibration_type, arg_tracking_base_frame, arg_tracking_marker_frame, arg_robot_base_frame, \
-    arg_robot_effector_frame
-
 
 def generate_launch_description():
-
     name = PythonExpression(['"easy_handeye2_demo_eih" if "eye_in_hand" == "',
                              LaunchConfiguration('calibration_type'),
-                             '" else "easy_handeye2_demo_eob"']),
+                             '" else "easy_handeye2_demo_eob"'])
+
+    tracking_base_frame = 'tracking_base'
+    tracking_marker_frame = 'tracking_marker'
+    robot_base_frame = 'panda_link0'
+    robot_effector_frame = 'panda_link8'
 
     incl_simulators = IncludeLaunchDescription(PythonLaunchDescriptionSource(
         os.path.join(get_package_share_directory('easy_handeye2_demo'), 'launch', 'robot_and_sim.launch.py')),
         launch_arguments={
             'name': name,
             'calibration_type': LaunchConfiguration('calibration_type'),
-            'tracking_base_frame': LaunchConfiguration('tracking_base_frame'),
-            'tracking_marker_frame': LaunchConfiguration('tracking_marker_frame'),
-            'robot_base_frame': LaunchConfiguration('robot_base_frame'),
-            'robot_effector_frame': LaunchConfiguration('robot_effector_frame'),
+            'tracking_base_frame': tracking_base_frame,
+            'tracking_marker_frame': tracking_marker_frame,
+            'robot_base_frame': robot_base_frame,
+            'robot_effector_frame': robot_effector_frame,
         }.items())
 
     incl_easy_handeye_calibrate = IncludeLaunchDescription(PythonLaunchDescriptionSource(
@@ -33,18 +35,26 @@ def generate_launch_description():
         launch_arguments={
             'name': name,
             'calibration_type': LaunchConfiguration('calibration_type'),
-            'tracking_base_frame': LaunchConfiguration('tracking_base_frame'),
-            'tracking_marker_frame': LaunchConfiguration('tracking_marker_frame'),
-            'robot_base_frame': LaunchConfiguration('robot_base_frame'),
-            'robot_effector_frame': LaunchConfiguration('robot_effector_frame'),
+            'tracking_base_frame': tracking_base_frame,
+            'tracking_marker_frame': tracking_marker_frame,
+            'robot_base_frame': robot_base_frame,
+            'robot_effector_frame': robot_effector_frame,
+            'automatic_robot_movement': LaunchConfiguration('automatic_robot_movement'),
+            'move_group': 'panda_arm',
+            'move_group_namespace': '',
+            'translation_delta_meters': LaunchConfiguration('translation_delta_meters'),
+            'rotation_delta_degrees': LaunchConfiguration('rotation_delta_degrees'),
+            'max_velocity_scaling': LaunchConfiguration('max_velocity_scaling'),
+            'max_acceleration_scaling': LaunchConfiguration('max_acceleration_scaling'),
         }.items())
 
     return LaunchDescription([
         arg_calibration_type,
-        arg_tracking_base_frame,
-        arg_tracking_marker_frame,
-        arg_robot_base_frame,
-        arg_robot_effector_frame,
+        arg_automatic_robot_movement,
+        arg_translation_delta_meters,
+        arg_rotation_delta_degrees,
+        arg_max_velocity_scaling,
+        arg_max_acceleration_scaling,
         incl_simulators,
         incl_easy_handeye_calibrate,
     ])
--- launch/check_calibration.launch.py
+++ launch/check_calibration.launch.py
@@ -6,8 +6,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression
 
-from easy_handeye2.common_launch import arg_calibration_type, arg_tracking_base_frame, arg_tracking_marker_frame, arg_robot_base_frame, \
-    arg_robot_effector_frame
+from easy_handeye2.common_launch import arg_calibration_type
 
 
 def generate_launch_description():
@@ -16,15 +15,20 @@ def generate_launch_description():
                              LaunchConfiguration('calibration_type'),
                              '" else "easy_handeye2_demo_eob"']),
 
+    tracking_base_frame = 'tracking_base'
+    tracking_marker_frame = 'tracking_marker'
+    robot_base_frame = 'panda_link0'
+    robot_effector_frame = 'panda_link8'
+
     incl_simulators = IncludeLaunchDescription(PythonLaunchDescriptionSource(
         os.path.join(get_package_share_directory('easy_handeye2_demo'), 'launch', 'robot_and_sim.launch.py')),
         launch_arguments={
             'name': name,
             'calibration_type': LaunchConfiguration('calibration_type'),
-            'tracking_base_frame': LaunchConfiguration('tracking_base_frame'),
-            'tracking_marker_frame': LaunchConfiguration('tracking_marker_frame'),
-            'robot_base_frame': LaunchConfiguration('robot_base_frame'),
-            'robot_effector_frame': LaunchConfiguration('robot_effector_frame'),
+            'tracking_base_frame': tracking_base_frame,
+            'tracking_marker_frame': tracking_marker_frame,
+            'robot_base_frame': robot_base_frame,
+            'robot_effector_frame': robot_effector_frame,
         }.items())
 
     incl_easy_handeye_publish = IncludeLaunchDescription(PythonLaunchDescriptionSource(
@@ -41,10 +45,6 @@ def generate_launch_description():
 
     return LaunchDescription([
         arg_calibration_type,
-        arg_tracking_base_frame,
-        arg_tracking_marker_frame,
-        arg_robot_base_frame,
-        arg_robot_effector_frame,
         incl_simulators,
         incl_easy_handeye_publish,
         incl_easy_handeye_evaluate,
