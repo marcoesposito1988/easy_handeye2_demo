15021684b76f37cb5f0616f873479b0b45653f1e
Marco Esposito <marcoesposito1988@gmail.com>
2025-01-25 15:19:57 +0100

    Added scripts to start a MoveIt docker container with X11 and to prepare a workspace for testing

---
+++ scripts/setup_workspace.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# this script will install packages and is supposed to be run within a docker container (e.g. with rocker and/or the start_docker.sh script next to this file)!
+
+rm -rf /tmp/easy_handeye_test
+mkdir -p !$
+cd !$
+
+mkdir -p easy_handeye2_test_ws/src
+cd easy_handeye2_test_ws/src
+
+git clone https://github.com/marcoesposito1988/easy_handeye2.git
+git clone https://github.com/marcoesposito1988/easy_handeye2_demo.git
+
+cd ..
+
+sudo apt update
+sudo apt install python3-pip
+rosdep update
+PIP_BREAK_SYSTEM_PACKAGES=1 rosdep install -yir --from-paths src
+
+colcon build --merge-install
+
---
+++ scripts/start_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$#" -eq  "0" ]
+then
+    ros_distro=jazzy
+    echo "No ROS distro specified; using ${ros_distro}"
+else
+    ros_distro=$1
+fi
+
+docker_image="moveit/moveit2:${ros_distro}-release"
+
+echo "Pulling and using docker image ${docker_image}"
+
+rocker --nvidia --x11 --user --pull ${docker_image}
